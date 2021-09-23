c3251a6d995ba5401ef68446c0e9252d4e7f4526
Martin Lenart <martin_lenart@icloud.com>
2021-09-23 14:11:53 +0200

    StructVsClass updated

--- DeckOfCards/Program.cs
+++ DeckOfCards/Program.cs
@@ -12,11 +12,44 @@ namespace DeckOfCards
         Knight, Queen, King, Ace                // Poker Value order
     }
 
+    #region could be a file
     public struct PlayingCard
     {
         public PlayingCardColor Color;
         public PlayingCardValue Value;
+
+        /// <summary>
+        /// Method that generates a string represting an object of type PlayingCard
+        /// </summary>
+        /// <returns>string that can be printed out using e.g., Console.WriteLine()</returns>
+        public string StringToPrint()
+        {
+            string sRet = $"Card is of color {Color} and value {Value}";
+            return sRet;
+        }
+
+        /*
+        public PlayingCard(PlayingCardColor color, PlayingCardValue value) 
+        {
+            Color = color;
+            Value = value;
+        }
+        public PlayingCard(bool myFavoriteOrWorst)
+        {
+            if (myFavoriteOrWorst)
+            {
+                Color = PlayingCardColor.Hearts;
+                Value = PlayingCardValue.Queen;
+            }
+            else
+            {
+                Color = PlayingCardColor.Clubs;
+                Value = PlayingCardValue.Two;
+            }
+        }
+        */
     }
+    #endregion
 
     class Program
     {
@@ -27,10 +60,21 @@ namespace DeckOfCards
 
             PlayingCard card1 = new PlayingCard { Color = PlayingCardColor.Clubs, Value = PlayingCardValue.Two };
             PlayingCard card2 = new PlayingCard { Color = PlayingCardColor.Clubs, Value = PlayingCardValue.Three };
+            
+            /*
+            PlayingCard card3 = new PlayingCard(PlayingCardColor.Hearts, PlayingCardValue.Four);
+            PlayingCard card4 = new PlayingCard(true);
+            Console.WriteLine(card4.StringToPrint());
+            */
+
             deckOfCards[0] = card1;
             deckOfCards[1] = card2;
 
-            #region init a freash deck
+            Console.WriteLine(card1.StringToPrint());
+            Console.WriteLine(card2.StringToPrint());
+
+            Console.WriteLine();
+            #region init a fresh deck
             //You can loop in a for-loop using an enum.
             //Remember int is the underlying type of an enum
             int cardNr1 = 0;
@@ -44,12 +88,15 @@ namespace DeckOfCards
 
                     //Alternative way working for PlayingCard being both class and struct
                     deckOfCards[cardNr1] = new PlayingCard { Color = color, Value = value };
+
+                    Console.WriteLine(deckOfCards[cardNr1].StringToPrint());
                     
                     //Prepare to initialize next card
                     cardNr1++;
                 }
             }
 
+            Console.WriteLine();
             //You can also use a foreach loop to initialize the deck of cards
             cardNr1 = 0;
             foreach (PlayingCardColor color in typeof(PlayingCardColor).GetEnumValues())
@@ -62,6 +109,8 @@ namespace DeckOfCards
 
                     //Alternative way working for PlayingCard being both class and struct
                     deckOfCards[cardNr1] = new PlayingCard { Color = color, Value = value };
+
+                    Console.WriteLine(deckOfCards[cardNr1].StringToPrint());
 
                     //Prepare to initialize next card
                     cardNr1++;
---
+++ StructVsReference/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StructVsReference
+{
+    public enum PlayingCardColor
+    {
+        Clubs, Diamonds, Hearts, Spades         // Poker suit order, Spades highest
+    }
+    public enum PlayingCardValue
+    {
+        Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten,
+        Knight, Queen, King, Ace                // Poker Value order
+    }
+
+    public struct PlayingCardStruct
+    {
+        public PlayingCardColor Color;
+        public PlayingCardValue Value;
+
+        public string StringToPrint()
+        {
+            string sRet = $"Card is of color {Color} and value {Value}";
+            return sRet;
+        }
+    }
+    public class PlayingCardClass
+    {
+        public PlayingCardColor Color;
+        public PlayingCardValue Value;
+
+        public string StringToPrint()
+        {
+            string sRet = $"Card is of color {Color} and value {Value}";
+            return sRet;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PlayingCardStruct sCard1;
+            PlayingCardStruct sCard2 = new PlayingCardStruct { Color = PlayingCardColor.Clubs, Value = PlayingCardValue.Two };
+            PlayingCardStruct sCard3 = new PlayingCardStruct { Color = PlayingCardColor.Clubs, Value = PlayingCardValue.Three };
+
+            PlayingCardClass cCard1;
+            PlayingCardClass cCard2 = new PlayingCardClass { Color = PlayingCardColor.Clubs, Value = PlayingCardValue.Two };
+            PlayingCardClass cCard3 = new PlayingCardClass { Color = PlayingCardColor.Clubs, Value = PlayingCardValue.Three };
+
+        }
+    }
+}
